7902a76a0ea2304d56cd29baae5f43acc2a99854
Fabio Cataldo <fabioalbertocataldo@gmail.com>
2025-03-20 19:16:28 -0300

    Implementation of app/seed/route.ts changed, due to db from vercel change

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -1,8 +1,9 @@
 import bcrypt from "bcryptjs";
 import postgres from "postgres";
 import { invoices, customers, revenue, users } from "../lib/placeholder-data";
+import { neon } from "@neondatabase/serverless";
 
-const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
+const sql = neon(`${process.env.DATABASE_URL}`);
 
 async function seedUsers() {
   await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
@@ -103,12 +104,11 @@ async function seedRevenue() {
 
 export async function GET() {
   try {
-    const result = await sql.begin((sql) => [
-      seedUsers(),
-      seedCustomers(),
-      seedInvoices(),
-      seedRevenue(),
-    ]);
+    await seedUsers();
+    await seedUsers();
+    await seedCustomers();
+    await seedInvoices();
+    await seedRevenue();
 
     return Response.json({ message: "Database seeded successfully" });
   } catch (error) {
